86a49f76981700ae03f44a66292fdd0cac1bc804
Taras Bovsunovskyi <taras2007zt@gmail.com>
2024-10-10 17:05:24 +0300

    Lab 1, 5, 7

---
+++ Lab7/Lab7.cpp
@@ -0,0 +1,16 @@
+﻿#include <stdio.h>
+#include <Windows.h>
+
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    double x, y;
+    printf("Введіть координати точки X: ");
+    scanf("%lf", &x);
+    printf("Введіть координати точки Y: ");
+    scanf("%lf", &y);
+    int result = (x >= 0 && y >= -x && y <= x) ? 1 : 0;
+    printf("Результат: %d\n", result);
+
+    return 0;
+}
